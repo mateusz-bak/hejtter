a9cda84bda8d1f08ccc7dfb1b0508f61d1bb65fa
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2022-12-22 12:50:43 +0000

    fix: Added high refresh rate

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:hejtter/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDisplayMode.setHighRefreshRate();
+  
   runApp(
     MaterialApp(
       home: const HomeScreen(),
