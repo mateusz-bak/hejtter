5558df44d644e9c2af694fd3f3076c8c61839cce
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2023-01-05 17:02:57 +0000

    build: Generated iOS files

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\mateu\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mateu\dev\moje\hejtter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.5"
+export "FLUTTER_BUILD_NUMBER=5"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
