cab8cb7788389cca65fd1e383abdf12780e8dd93
mateusz-bak <32651935+mateusz-bak@users.noreply.github.com>
2023-01-06 22:43:57 +0000

    feat: Disabling NSFW blur will work on posts

--- lib/ui/post_screen/picture_preview.dart
+++ lib/ui/post_screen/picture_preview.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hejtter/logic/bloc/profile_bloc/profile_bloc.dart';
 import 'package:hejtter/models/posts_response.dart';
 import 'package:hejtter/ui/post_screen/picture_full_screen.dart';
 import 'package:hejtter/utils/constants.dart';
@@ -51,20 +53,7 @@ class _PicturePreviewState extends State<PicturePreview> {
                 ),
               ),
             ),
-            widget.nsfw && _hideNsfw
-                ? Positioned.fill(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(3),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-                        child: Container(
-                          color: Colors.black.withOpacity(0.2),
-                          alignment: Alignment.center,
-                        ),
-                      ),
-                    ),
-                  )
-                : const SizedBox(),
+            widget.nsfw && _hideNsfw ? _buildNsfwBlur() : const SizedBox(),
             widget.multiplePics
                 ? Padding(
                     padding: const EdgeInsets.all(10),
@@ -81,35 +70,70 @@ class _PicturePreviewState extends State<PicturePreview> {
                     ),
                   )
                 : const SizedBox(),
-            widget.nsfw
-                ? Align(
-                    alignment: Alignment.topRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _hideNsfw = !_hideNsfw;
-                          });
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: backgroundColor.withAlpha(150),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Icon(
-                            _hideNsfw ? Icons.visibility : Icons.visibility_off,
-                            size: 20,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
-                : const SizedBox(),
+            widget.nsfw ? _buildNsfwButton() : const SizedBox(),
           ],
         ),
       ),
+    );
+  }
+
+  _buildNsfwBlur() {
+    return BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+      if (state is ProfilePresentState) {
+        if (!state.blurNsfw) {
+          return const SizedBox();
+        }
+      }
+
+      return Positioned.fill(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(3),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+            child: Container(
+              color: Colors.black.withOpacity(0.2),
+              alignment: Alignment.center,
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  _buildNsfwButton() {
+    return BlocBuilder<ProfileBloc, ProfileState>(
+      builder: (context, state) {
+        if (state is ProfilePresentState) {
+          if (!state.blurNsfw) {
+            return const SizedBox();
+          }
+        }
+
+        return Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _hideNsfw = !_hideNsfw;
+                });
+              },
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: backgroundColor.withAlpha(150),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(
+                  _hideNsfw ? Icons.visibility : Icons.visibility_off,
+                  size: 20,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
